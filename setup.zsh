6b2992cab8dbbfde767a41f0b19cdc0f418bb1a8
Alex Lapinski <contact@alexlapinski.name>
2019-04-24 22:26:49 -0400

    Added more details

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
-
+#
+# Loosely based on the following tutorial
+# https://cloud.google.com/community/tutorials/managing-gcp-projects-with-terraform
+#
 # TODO: Make sure gcloud is setup for bash
+# TODO: Convert this back to a 'Bash' script
 
 echo "# Setting up environment variables\n"
 TF_VAR_billing_account=$(
@@ -18,4 +22,36 @@ echo "TF_CREDS = $TF_CREDS"
 
 echo "\n\xE2\x9C\x94 Done\n"
 
-# gcloud 
+echo "# Creating Terraform Admin project"
+gcloud projects create ${TF_ADMIN} \
+  --set-as-default
+echo "\n\xE2\x9C\x94 Done\n"
+
+echo "# Setting default billing account"
+gcloud beta billing projects link ${TF_ADMIN} \
+  --billing-account ${TF_VAR_billing_account}
+echo "\n\xE2\x9C\x94 Done\n"
+
+
+echo "# Creating Terraform Service Account"
+gcloud iam service-accounts create terraform \
+  --display-name "Terraform admin account"
+
+gcloud iam service-accounts keys create ${TF_CREDS} \
+  --iam-account terraform@${TF_ADMIN}.iam.gserviceaccount.com
+echo "\n\xE2\x9C\x94 Done\n"
+
+echo "# Granting the service account permissions"
+gcloud projects add-iam-policy-binding ${TF_ADMIN} \
+  --member serviceAccount:terraform@${TF_ADMIN}.iam.gserviceaccount.com \
+  --role roles/viewer
+
+gcloud projects add-iam-policy-binding ${TF_ADMIN} \
+  --member serviceAccount:terraform@${TF_ADMIN}.iam.gserviceaccount.com \
+  --role roles/storage.admin
+
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable cloudbilling.googleapis.com
+gcloud services enable iam.googleapis.com
+gcloud services enable compute.googleapis.com
+echo "\n\xE2\x9C\x94 Done\n"
